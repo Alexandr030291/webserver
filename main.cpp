a29b9296badbf42822fdfc7bdeb0e45c6a5f8ada
Alexandr030291 <DontSmoke030291>
2017-05-18 00:30:30 +0300

    Добавден поток обработки команд, доступная команда "STOP" - остановить программу

--- main.cpp
+++ main.cpp
@@ -6,7 +6,26 @@
 #define MAX_THREAD_COUNT 4
 #define RUN_TIMEOUT 10
 
+
+void * controller(void *arg){
+    bool stop = !(bool) arg;
+    const int max_buffer = 2048;
+    const char STOP[] = "STOP";
+    char buffer[max_buffer]= {'\0'};
+
+    while(stop){
+        scanf("%s",buffer);
+        if (strcmp(buffer,STOP)==0){
+            stop=!stop;
+            EpollEngine::g_Stop = true;
+        }
+    }
+    return nullptr;
+}
+
 int main(int argc, char* argv[]) {
+    pthread_t command;
+    pthread_create(&command, NULL, controller, false);
     Server epoll_server((char *) SERVER_HOST, SERVER_PORT, MAX_THREAD_COUNT, MAX_QUERY_SIZE, RUN_TIMEOUT);
     epoll_server.run();
     return 0;
